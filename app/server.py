30c302b7fffd9cde39f964ed1faffbc18f8e821b
Jelani Woods <jelani@firsdraft.com>
2019-03-15 13:17:59 -0500

    Fix deprecation errors

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
-model_file_name = 'model'
+export_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+export_file_name = 'export.pkl'
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
@@ -25,11 +25,8 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
-    learn.load(model_file_name)
+    await download_file(export_file_url, path/export_file_name)
+    learn = load_learner(path, export_file_name)
     return learn
 
 loop = asyncio.get_event_loop()
@@ -51,4 +48,3 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
-
